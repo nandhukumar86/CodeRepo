497c85ff9400fb95cce04e6bd74c5f7cad1f02cf
Nandhakumar <nandhu_kumar86@yahoo.com>
2021-01-26 16:14:16 +0530

    Proxy

---
+++ ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerProxyToLog server = new ServerProxyToLog();
+            server.HandleRequest();
+        }
+
+
+        /*
+         * Proxy Pattern
+         * -------------
+         * Copy the functionality of the Subject and do some modifications and use the new subject
+         * 
+         * Ex. Server is handling a request, but we need to introduce logging when the server is handling it.
+         * 
+         */
+
+        public interface IServer
+        {
+            void HandleRequest();
+        }
+
+        public class Server : IServer
+        {
+            public void HandleRequest()
+            {
+                Console.WriteLine("Server is Handling the Request");
+            }
+        }
+
+
+        // 1. Create Proxy Server
+        public class ServerProxyToLog : IServer
+        {
+            Server server = new Server();
+            public void HandleRequest()
+            {
+                Console.WriteLine($"Logging request at {DateTime.Now}");
+                server.HandleRequest();
+
+            }
+        }
+
+    }
+}
